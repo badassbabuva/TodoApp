b81d99ee58fdafa7a48308bdcffbbf5442aa6b97
Priyansh Parmar <91priyansh@gmail.com>
2018-06-02 12:14:46 +0530

    Heroku MongoDb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/CricketDetails');
+mongoose.connect('mongodb://admin:piyu123@ds245680.mlab.com:45680/todoapp');
 
 module.exports = {mongoose};

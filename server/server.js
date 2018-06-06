b9932fc542ddbd701d024a7c62d006ccc0630ba9
Priyansh Parmar <91priyansh@gmail.com>
2018-06-06 11:19:05 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 //const hbs = require('hbs');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
@@ -14,6 +15,7 @@ app.set('view engine','hbs');
 
 app.use(bodyParser.json());
 
+//post todos
 app.post('/todos', (req,res) => {
 
 	var todo = new Todo({
@@ -33,6 +35,7 @@ app.post('/todos', (req,res) => {
 	
 });
 
+// get todos
 app.get('/todos',(req,res)=>{
 
 	Todo.find().then((todos) => {
@@ -48,6 +51,7 @@ app.get('/todos',(req,res)=>{
 
 });
 
+//post user
 app.post('/user',(req,res) => {
 
 	var body = _.pick(req.body,['email','password']);
@@ -62,19 +66,49 @@ app.post('/user',(req,res) => {
 
 		return user.generateAuthToken();
 
-	},(err)=>{
-
-		res.status(400).send(err);
-
 	}).then((token) => {
 		res.header('x-auth',token).send(user);
-	},(err)=> {
-	
-		res.status(400).send(err);		
+	}).catch((e) => {
+		
+		res.send(e);
+
 	});
 
 });
 
+//user login
+
+app.post('/user/login',(req,res) => {
+
+	var body = _.pick(req.body,['email','password']);
+
+	User.findOne({email: body.email}).then((user) => {
+				
+			if(!user){
+				res.send("Invalid email id");
+			}
+			else{
+				bcrypt.compare(body.password,user.password,(err,result)=>{
+
+					if(result){
+
+						res.send("password is correct");
+
+					}else
+					{
+						res.send("password is incorrect");
+					}
+				});
+
+			}
+				
+	}).catch((e) => {
+		res.status(404).send(e);
+	});
+});
+
+
+//get todo/s by id
 app.get('/todos/:id',(req,res) => {
 
  	var id = req.params.id;
@@ -104,6 +138,7 @@ app.get('/todos/:id',(req,res) => {
 
 });
 
+//delete todo by id
 app.delete('/remove/:id',(req,res) => {
 
  	var id = req.params.id;
@@ -133,6 +168,7 @@ app.delete('/remove/:id',(req,res) => {
 
 });
 
+//update todo by id
 app.patch('/todos/:id',(req,res) => {
 
 	var id = req.params.id;
@@ -159,6 +195,7 @@ app.patch('/todos/:id',(req,res) => {
 
 });
 
+//autenticate 
 var authenticate = (req,res,next) => {
 
 	var token = req.header('x-auth');
@@ -183,6 +220,7 @@ var authenticate = (req,res,next) => {
 
 };
 
+// find user by token
 app.get('/user/me',authenticate,(req,res) => {
 
 	res.send(req.user);
